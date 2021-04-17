c0944b162fbecbf18ce1d1e388c7974f895223eb
Juan <juanrodriguezgiles@gmail.com>
2021-04-17 19:17:05 -0300

    Implemented scene switching to game manager

--- tsdv_tp04_juanrodriguez/Assets/Scripts/GameManager.cs
+++ tsdv_tp04_juanrodriguez/Assets/Scripts/GameManager.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int score;
+    bool playing = true;
+    int shotsLeft = 3;
+    int pinsLeft = 10;
+
+    private static GameManager instance;
+    public static GameManager Get()
     {
-        
+        return instance;
     }
-    // Update is called once per frame
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     void Update()
     {
-        
+
     }
+
+    #region SCENES
+    public void LoadBowlingGameplayScene()
+    {
+        SceneManager.LoadScene("GameplayBowling");
+    }
+    public void LoadGunGameplayScene()
+    {
+        SceneManager.LoadScene("GameplayGun");
+    }
+    public void LoadScoreScreenScene()
+    {
+        SceneManager.LoadScene("ScoreScreen");
+    }
+    #endregion
+
 }
